6ba599799de451d19850826fa75a471a1d6df620
Dave <d.ruocco1987@googlemail.com>
2022-06-03 20:17:46 +0100

    Added conditional to badgeText variable, cleaned up Card component, centred card display div

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,26 @@ import React from "react";
 import starIcon from './img/star.png';
 
 const Card = (props) => {
+
+    let badgeText;
+    if (props.item.openSpots === 0 ) {
+        badgeText = 'SOLD OUT';
+    } else if (props.item.location === 'Online') {
+        badgeText = 'ONLINE';
+    };
+
     return (
             <div className="card">
-                {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
-                <img src={`../img/${props.img}`} className="card--image" />
+                {badgeText && <div className="card--badge">{badgeText}</div>} 
+                <img src={`../img/${props.item.coverImg}`} className="card--image" />
                 <div className="card--stats">
                     <img className="card--star" src={starIcon} />
-                    <span>{props.rating}</span>
-                    <span className="grey">({props.reviewCount})• </span>
-                    <span className="grey">{props.country}</span>
+                    <span>{props.item.stats.rating}</span>
+                    <span className="grey">({props.item.stats.reviewCount})• </span>
+                    <span className="grey">{props.item.location}</span>
                 </div>
-                    <p className="card--title">{props.title}</p>
-                    <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+                    <p className="card--title">{props.item.title}</p>
+                    <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
             </div>
     )
 };
